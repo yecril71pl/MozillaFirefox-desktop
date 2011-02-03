3d90f551390a25c166d60d1286afb092fef6fead
Wolfgang Rosenauer <wr@rosenauer.org>
2011-02-03 10:42:10 +0100

    update for 2.0b11/4.0b11 - added mozilla-cairo-return.patch - rebased patches

--- mozilla-xulrunner20/create-tar.sh
+++ mozilla-xulrunner20/create-tar.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 BRANCH="mozilla-central"
-RELEASE_TAG="FIREFOX_4_0b10_RELEASE"
-VERSION="2.0b10"
+RELEASE_TAG="FIREFOX_4_0b11_RELEASE"
+VERSION="2.0b11"
 
 # mozilla
 hg clone http://hg.mozilla.org/$BRANCH mozilla
@@ -14,7 +14,7 @@ tar cjf xulrunner-source-$VERSION.tar.bz2 --exclude=.hgtags --exclude=.hgignore 
 # l10n
 test ! -d l10n && mkdir l10n
 for locale in $(awk '{ print $1; }' mozilla/browser/locales/shipped-locales); do
-  case $locale in 
+  case $locale in
     ja-JP-mac|en-US)
       ;;
     *)

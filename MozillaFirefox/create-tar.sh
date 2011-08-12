a51015d7d49b21dbdebaaded59b24b2eafa12fb3
Wolfgang Rosenauer <wr@rosenauer.org>
2011-08-12 23:56:40 +0200

    update to 6.0

--- MozillaFirefox/create-tar.sh
+++ MozillaFirefox/create-tar.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-BRANCH="releases/mozilla-beta"
-RELEASE_TAG="FIREFOX_6_0b5_RELEASE"
-VERSION="5.99"
+BRANCH="releases/mozilla-release"
+RELEASE_TAG="FIREFOX_6_0_RELEASE"
+VERSION="6.0"
 
 # mozilla
 hg clone http://hg.mozilla.org/$BRANCH mozilla
@@ -23,7 +23,7 @@ for locale in $(awk '{ print $1; }' mozilla/browser/locales/shipped-locales); do
     ja-JP-mac|en-US)
       ;;
     *)
-      hg clone http://hg.mozilla.org/releases/l10n/mozilla-beta/$locale l10n/$locale
+      hg clone http://hg.mozilla.org/releases/l10n/mozilla-release/$locale l10n/$locale
       [ "$RELEASE_TAG" == "default" ] || hg -R l10n/$locale up -C -r $RELEASE_TAG
       ;;
   esac

148b64499b30b778b5cdd1833eec9529b87e1712
Wolfgang Rosenauer <wr@rosenauer.org>
2011-12-23 18:44:50 +0100

    9.0.1

--- MozillaFirefox/create-tar.sh
+++ MozillaFirefox/create-tar.sh
@@ -2,8 +2,8 @@
 
 CHANNEL="release"
 BRANCH="releases/mozilla-$CHANNEL"
-RELEASE_TAG="FIREFOX_9_0_RELEASE"
-VERSION="9.0"
+RELEASE_TAG="FIREFOX_9_0_1_RELEASE"
+VERSION="9.0.1"
 
 # mozilla
 hg clone http://hg.mozilla.org/$BRANCH mozilla

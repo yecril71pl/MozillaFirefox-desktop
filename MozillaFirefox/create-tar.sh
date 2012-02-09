d0edbf1619c607a14b99475966c01281cc051c3e
Wolfgang Rosenauer <wr@rosenauer.org>
2012-02-09 07:34:00 +0100

    10.0.1

---
+++ MozillaFirefox/create-tar.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+CHANNEL="release"
+BRANCH="releases/mozilla-$CHANNEL"
+RELEASE_TAG="FIREFOX_10_0_1_RELEASE"
+VERSION="10.0.1"
+
+# mozilla
+hg clone http://hg.mozilla.org/$BRANCH mozilla
+pushd mozilla
+[ "$RELEASE_TAG" == "default" ] || hg update -r $RELEASE_TAG
+# get repo and source stamp
+echo -n "REV=" > ../source-stamp.txt
+hg -R . parent --template="{node|short}\n" >> ../source-stamp.txt
+echo -n "REPO=" >> ../source-stamp.txt
+hg showconfig paths.default 2>/dev/null | head -n1 | sed -e "s/^ssh:/http:/" >> ../source-stamp.txt
+popd
+tar cjf firefox-$VERSION-source.tar.bz2 --exclude=.hgtags --exclude=.hgignore --exclude=.hg --exclude=CVS mozilla
+
+# l10n
+test ! -d l10n && mkdir l10n
+for locale in $(awk '{ print $1; }' mozilla/browser/locales/shipped-locales); do
+  case $locale in
+    ja-JP-mac|en-US)
+      ;;
+    *)
+      hg clone http://hg.mozilla.org/releases/l10n/mozilla-$CHANNEL/$locale l10n/$locale
+      [ "$RELEASE_TAG" == "default" ] || hg -R l10n/$locale up -C -r $RELEASE_TAG
+      ;;
+  esac
+done
+tar cjf l10n-$VERSION.tar.bz2 --exclude=.hgtags --exclude=.hgignore --exclude=.hg l10n
+
+# compare-locales
+hg clone http://hg.mozilla.org/build/compare-locales
+tar cjf compare-locales.tar.bz2 --exclude=.hgtags --exclude=.hgignore --exclude=.hg compare-locales
+

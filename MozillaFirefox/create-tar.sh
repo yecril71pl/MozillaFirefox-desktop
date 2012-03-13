96eec40ca2eb345adc4452407a6221a91b0b6ecb
Wolfgang Rosenauer <wr@rosenauer.org>
2012-03-13 08:06:57 +0100

    Firefox 11 release

--- MozillaFirefox/create-tar.sh
+++ MozillaFirefox/create-tar.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-CHANNEL="beta"
+CHANNEL="release"
 BRANCH="releases/mozilla-$CHANNEL"
-RELEASE_TAG="FIREFOX_11_0b6_RELEASE"
-VERSION="10.99"
+RELEASE_TAG="FIREFOX_11_0_RELEASE"
+VERSION="11.0"
 
 # mozilla
 hg clone http://hg.mozilla.org/$BRANCH mozilla

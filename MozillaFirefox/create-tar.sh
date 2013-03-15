ca8b26056d764b3deff037ee736babe2b740de95
Wolfgang Rosenauer <wr@rosenauer.org>
2013-03-16 00:07:21 +0100

    19.0.2 arm fixes

--- MozillaFirefox/create-tar.sh
+++ MozillaFirefox/create-tar.sh
@@ -2,8 +2,8 @@
 
 CHANNEL="release"
 BRANCH="releases/mozilla-$CHANNEL"
-RELEASE_TAG="FIREFOX_19_0_1_RELEASE"
-VERSION="19.0.1"
+RELEASE_TAG="FIREFOX_19_0_2_RELEASE"
+VERSION="19.0.2"
 
 # mozilla
 echo "cloning $BRANCH..."

ca043402eeb2dde0103627ad5882481ab2f1d869
Wolfgang Rosenauer <wr@rosenauer.org>
2013-10-24 18:41:12 +0200

    17.0.10esr update

--- xulrunner/create-tar.sh
+++ xulrunner/create-tar.sh
@@ -2,8 +2,8 @@
 
 CHANNEL="esr17"
 BRANCH="releases/mozilla-$CHANNEL"
-RELEASE_TAG="FIREFOX_17_0_9esr_RELEASE"
-VERSION="17.0.9"
+RELEASE_TAG="FIREFOX_17_0_10esr_RELEASE"
+VERSION="17.0.10"
 
 # mozilla
 echo "cloning $BRANCH..."

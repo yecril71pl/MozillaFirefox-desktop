31fec3b8df72812309416d78a01f16fa74503f87
Wolfgang Rosenauer <wr@rosenauer.org>
2014-02-05 07:16:15 +0100

    24.3.0 release

--- MozillaFirefox/create-tar.sh
+++ MozillaFirefox/create-tar.sh
@@ -2,8 +2,8 @@
 
 CHANNEL="esr24"
 BRANCH="releases/mozilla-$CHANNEL"
-RELEASE_TAG="FIREFOX_24_2_0esr_RELEASE"
-VERSION="24.2.0"
+RELEASE_TAG="FIREFOX_24_3_0esr_RELEASE"
+VERSION="24.3.0"
 
 # mozilla
 if [ -d mozilla ]; then

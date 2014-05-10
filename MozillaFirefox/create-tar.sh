56ceb0fdd0646ae6c80f9f631a15304bd43803a1
Wolfgang Rosenauer <wr@rosenauer.org>
2014-05-10 08:13:15 +0200

    29.0.1 release

--- MozillaFirefox/create-tar.sh
+++ MozillaFirefox/create-tar.sh
@@ -2,8 +2,8 @@
 
 CHANNEL="release"
 BRANCH="releases/mozilla-$CHANNEL"
-RELEASE_TAG="FIREFOX_29_0_RELEASE"
-VERSION="29.0"
+RELEASE_TAG="FIREFOX_29_0_1_RELEASE"
+VERSION="29.0.1"
 
 # mozilla
 if [ -d mozilla ]; then

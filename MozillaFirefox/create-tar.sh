33066ffe98ee29bb8b7555f3880627eb7397f05e
Wolfgang Rosenauer <wr@rosenauer.org>
2015-04-07 14:55:00 +0200

    Firefox 37.0.1

--- MozillaFirefox/create-tar.sh
+++ MozillaFirefox/create-tar.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-CHANNEL="beta"
+CHANNEL="release"
 BRANCH="releases/mozilla-$CHANNEL"
-RELEASE_TAG="FIREFOX_37_0b7_RELEASE"
-VERSION="36.99"
+RELEASE_TAG="FIREFOX_37_0_1_RELEASE"
+VERSION="37.0.1"
 
 # mozilla
 if [ -d mozilla ]; then

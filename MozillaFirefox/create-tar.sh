6dfc7bf511d6e051a875252ac20bee1f8628b585
Wolfgang Rosenauer <wr@rosenauer.org>
2015-06-27 17:28:16 +0200

    31.8.0esr release

--- MozillaFirefox/create-tar.sh
+++ MozillaFirefox/create-tar.sh
@@ -2,8 +2,8 @@
 
 CHANNEL="esr31"
 BRANCH="releases/mozilla-$CHANNEL"
-RELEASE_TAG="FIREFOX_31_7_0esr_RELEASE"
-VERSION="31.7.0"
+RELEASE_TAG="FIREFOX_31_8_0esr_RELEASE"
+VERSION="31.8.0"
 
 # mozilla
 if [ -d mozilla ]; then

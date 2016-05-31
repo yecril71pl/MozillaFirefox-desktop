bf25a8cafeebbfcc5746418a25d47f6e453405db
Wolfgang Rosenauer <wr@rosenauer.org>
2016-05-31 11:18:55 +0200

    38.8

--- MozillaFirefox/create-tar.sh
+++ MozillaFirefox/create-tar.sh
@@ -2,8 +2,8 @@
 
 CHANNEL="esr38"
 BRANCH="releases/mozilla-$CHANNEL"
-RELEASE_TAG="FIREFOX_38_7_0esr_RELEASE"
-VERSION="38.7.0"
+RELEASE_TAG="FIREFOX_38_8_0esr_RELEASE"
+VERSION="38.8.0"
 
 # mozilla
 if [ -d mozilla ]; then

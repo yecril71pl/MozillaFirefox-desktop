cdd4de72d06931f6b8a91be8085ca9ab9009ad20
Wolfgang Rosenauer <wr@rosenauer.org>
2017-01-22 17:41:41 +0100

    switch from beta to release channel for 51.0build2

--- MozillaFirefox/create-tar.sh
+++ MozillaFirefox/create-tar.sh
@@ -5,10 +5,10 @@
 # "moz_source_stamp": "c1de04f39fa956cfce83f6065b0e709369215ed5"
 # http://ftp.mozilla.org/pub/firefox/candidates/48.0-candidates/build2/l10n_changesets.txt
 
-CHANNEL="beta"
+CHANNEL="release"
 BRANCH="releases/mozilla-$CHANNEL"
-RELEASE_TAG="FIREFOX_51_0b14_RELEASE"
-VERSION="50.99"
+RELEASE_TAG="ea82b5e20cbbd103f8fa65f0df0386ee4135cc47"
+VERSION="51.0"
 
 # mozilla
 if [ -d mozilla ]; then
